3e3e335e1cd77c352034b6cfed82a4fe4412cb29
szampier <56970291+szampier@users.noreply.github.com>
2024-10-30 16:12:46 +0000

    cfitsio@4.2.0: update 4.2.0_1 bottle.

--- Formula/cfitsio@4.2.0.rb
+++ Formula/cfitsio@4.2.0.rb
@@ -12,10 +12,11 @@ class CfitsioAT420 < Formula
   end
 
   bottle do
-    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/cfitsio@4.2.0-4.2.0"
-    sha256 cellar: :any,                 arm64_sonoma: "6cc4896c2e06522f32c28948956526d9a23660ad0545ec1123b64e67d6daee9f"
-    sha256 cellar: :any,                 ventura:      "de56e7ead31473d8ac02f8a498c9af50355fe6f0f39e2bdfe9388f73fd9666d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "52973416c34b3876b717809ec5bb3e9f816579916d9ecaa430e39b3eb44e0242"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/cfitsio@4.2.0-4.2.0_1"
+    sha256 cellar: :any,                 arm64_sequoia: "f2549f852e5b85258bef12a26c1c6684d3f95c8a32bf13f454a2f5e7aa9af498"
+    sha256 cellar: :any,                 arm64_sonoma:  "6197ead536490170a7dcdaab291905615a7e940f0d819e2a56af5d9aa068db82"
+    sha256 cellar: :any,                 ventura:       "64e470a67f23981c596ea63fa23de3e1e292ca84f2f0a4e8245dd3e054e02520"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa2ef7284df3a8f7cac8719e2d27aa435238ff6178a4f597d490c085410359f4"
   end
 
   keg_only :versioned_formula
