31eb4fc6107f1692de22dd15b4b276c0e07e0f7d
Stefano Zampieri <szampier@eso.org>
2024-10-30 21:26:32 +0100

    esopipe-spher-recipes: revision bump
    Closes #32.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-spher-recipes.rb
+++ Formula/esopipe-spher-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeSpherRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.56.0.tar.gz"
   sha256 "0c06cfa743b85fbf2980d0dbdb1bcf8cf57ea591e1f1cec987d6ca4abdc25157"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
