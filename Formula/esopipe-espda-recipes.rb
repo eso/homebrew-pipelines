47de5f3145c87c335eac4c3488d170789aa55a6a
Stefano Zampieri <szampier@eso.org>
2024-10-30 21:24:18 +0100

    esopipe-espda-recipes: revision bump

--- Formula/esopipe-espda-recipes.rb
+++ Formula/esopipe-espda-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeEspdaRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso-das/espda-kit-1.3.8.tar.gz"
   sha256 "d100d9c4318fcfd853f445413ee5a73ad8ca5f41f359ea016afae3758e7e33d6"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
