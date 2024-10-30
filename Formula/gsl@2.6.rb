b1161d178b2ba3fdf60ee9e8867ae9845812116e
szampier <56970291+szampier@users.noreply.github.com>
2024-10-30 16:12:51 +0000

    gsl@2.6: update 2.6_2 bottle.

--- Formula/gsl@2.6.rb
+++ Formula/gsl@2.6.rb
@@ -8,10 +8,11 @@ class GslAT26 < Formula
   revision 2
 
   bottle do
-    root_url "https://github.com/szampier/homebrew-pipelines/releases/download/gsl@2.6-2.6_1"
-    sha256 cellar: :any,                 arm64_sonoma: "716da03e02350f76b1ea2cde0721b4e9f8df67aab554ec063e5c9bb5eff0966c"
-    sha256 cellar: :any,                 ventura:      "d58eb8d03ec3772b7cd688103ea894e40597181edf02faa3795503586db61e70"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "70b47bd9876a48723d21b91af0763e6613e20436481d97910aaa12361b8a55da"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/gsl@2.6-2.6_2"
+    sha256 cellar: :any,                 arm64_sequoia: "b60ae71df9ab631119e630f69bf3bc073706f621da65d41e95af63b0f51ff704"
+    sha256 cellar: :any,                 arm64_sonoma:  "6eee3bb47ca48d7db3d77251b32bfa352c0cc3abe3d3b5954382de59e1bb8936"
+    sha256 cellar: :any,                 ventura:       "9a470f3387eebcac2f8ec82e1d60df2a4838a7f6a6b234018d019138c2264de7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e89d1241417cae0d62fb4a4accdaf1feca1f61a751171ea5cec4ce7ab0385756"
   end
 
   keg_only :versioned_formula
