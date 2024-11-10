9a2af1b52e014f9f77a03118233b1bfb0fc5ebb0
Stefano Zampieri <szampier@eso.org>
2024-11-10 21:16:49 +0100

    update both copies of the workflow

--- Formula/esopipe-uves-recipes.rb
+++ Formula/esopipe-uves-recipes.rb
@@ -43,12 +43,14 @@ class EsopipeUvesRecipes < Formula
   end
 
   def post_install
-    workflow_dir = prefix/"share/reflex/workflows/uves-#{version.major_minor_patch}"
-    workflow_dir.glob("*.xml").each do |workflow|
+    workflow_dir_1 = prefix/"share/reflex/workflows/uves-#{version.major_minor_patch}"
+    workflow_dir_2 = prefix/"share/esopipes/uves-#{version.major_minor_patch}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
       ohai "Updating [ROOT|CALIB|RAW]_DATA_DIR in #{workflow}"
       inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
       inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
       inreplace workflow, "$ROOT_DATA_DIR/reflex_input", "#{HOMEBREW_PREFIX}/share/esopipes/datademo"
+      cp workflow, workflow_dir_2
     end
   end
 
