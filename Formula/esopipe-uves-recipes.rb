8f21b06dccc980fb73dcfd71c0efc45df65d9ac0
Stefano Zampieri <szampier@eso.org>
2024-11-13 12:35:34 +0100

    esopipe-uves-recipes: revision bump
    Closes #38.
    
    Signed-off-by: szampier <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-uves-recipes.rb
+++ Formula/esopipe-uves-recipes.rb
@@ -4,7 +4,7 @@ class EsopipeUvesRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.6.tar.gz"
   sha256 "0d80c8cd55a271f2cb31549483ee139ac2e8054c759825d56bb605c41779a10a"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   def name_version
     "uves-#{version.major_minor_patch}"
