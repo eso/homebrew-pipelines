eac0f5c492387b58e5c02cab64df09bfffdedc86
szampier <56970291+szampier@users.noreply.github.com>
2024-11-25 10:12:31 +0000

    esopipe-harps: add 3.3.0 bottle.

--- Formula/esopipe-harps.rb
+++ Formula/esopipe-harps.rb
@@ -10,6 +10,14 @@ class EsopipeHarps < Formula
     regex(/href=.*?harps-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-harps-3.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "40d746ba6a33533c751712a45a258d9e39323e5e84121bcdd7fb06dee8f496ea"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c69fd8181e948e1371ccf3bfda668b41b7ccdb4135147b2b2af25893670cc91"
+    sha256 cellar: :any_skip_relocation, ventura:       "79a3310d2cfd91e49b46f2d2566edd50a79946cab60e17e92baf7097ca8baf1b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "abdb5bd68999af29f0afb9e4a647618a55eec30e707a0330ef2b478ea89fb4e8"
+  end
+
   depends_on "esopipe-harps-recipes"
 
   def install
