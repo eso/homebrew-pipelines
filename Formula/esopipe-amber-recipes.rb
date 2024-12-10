94cab2b0fea3900537432bacdfb5cd9960061854
Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>
2024-12-10 10:45:27 +0000

    esopipe-amber-recipes: add 4.4.5-1 bottle.

--- Formula/esopipe-amber-recipes.rb
+++ Formula/esopipe-amber-recipes.rb
@@ -5,6 +5,14 @@ class EsopipeAmberRecipes < Formula
   sha256 "ab1321479850c42c2eb0f24966dbe91b994cb48e1ccc99f8722206edcc5cca3b"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-amber-recipes-4.4.5-1"
+    sha256 cellar: :any,                 arm64_sequoia: "a235e75cc5a3be2e963c3ce544a10b86ca9c87d8d29b073935921205954a0aa8"
+    sha256 cellar: :any,                 arm64_sonoma:  "64095397e255639e3270c03f8a648123c3c49f48db1fb6353e475ceba0cb3e47"
+    sha256 cellar: :any,                 ventura:       "d8a9add84dc682c434b710079e915a7dae6265f53efb7c4a5542d1001e29e247"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e8175fffa3b5b14ac605c2382a45b2bab1883a46a97233a4e920c062921b1d2"
+  end
+
   def name_version
     "amber-#{version.major_minor_patch}"
   end
