60fa98c7c619b0743f8917709b53a69dbefb8782
Stefano Zampieri <szampier@eso.org>
2024-12-17 16:56:47 +0100

    esopipe-kmos 4.4.8

--- Formula/esopipe-kmos.rb
+++ Formula/esopipe-kmos.rb
@@ -1,8 +1,8 @@
 class EsopipeKmos < Formula
   desc "ESO KMOS instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/kmos/kmos-kit-4.4.4.tar.gz"
-  sha256 "455b58e777335f59f31e7af56d77d64f54ea8a9f58dedae9851fbf75864de612"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/kmos/kmos-kit-4.4.8.tar.gz"
+  sha256 "153443b61944c736972c389dc9e1c38df5a8b674ac35f2c8846ee9b96c25dd4a"
   license "GPL-2.0-or-later"
 
   livecheck do
