8dca52e360a629753f9691b8a1de4607fe535395
Stefano Zampieri <szampier@eso.org>
2024-12-17 16:57:52 +0100

    esopipe-uves-recipes 6.4.10

--- Formula/esopipe-uves-recipes.rb
+++ Formula/esopipe-uves-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeUvesRecipes < Formula
   desc "ESO UVES instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.6.tar.gz"
-  sha256 "0d80c8cd55a271f2cb31549483ee139ac2e8054c759825d56bb605c41779a10a"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.10.tar.gz"
+  sha256 "57fc759cec5d42a146ace61c6b8b423144ed0d5968b10dafaa45b5612fe78960"
   license "GPL-2.0-or-later"
-  revision 4
 
   def name_version
     "uves-#{version.major_minor_patch}"
