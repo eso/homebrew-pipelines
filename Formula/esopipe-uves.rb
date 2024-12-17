4593907d0fde4f41f6eae3f784b00b132091001b
Stefano Zampieri <szampier@eso.org>
2024-12-17 16:57:36 +0100

    esopipe-uves 6.4.10

--- Formula/esopipe-uves.rb
+++ Formula/esopipe-uves.rb
@@ -1,8 +1,8 @@
 class EsopipeUves < Formula
   desc "ESO UVES instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.6.tar.gz"
-  sha256 "0d80c8cd55a271f2cb31549483ee139ac2e8054c759825d56bb605c41779a10a"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.10.tar.gz"
+  sha256 "57fc759cec5d42a146ace61c6b8b423144ed0d5968b10dafaa45b5612fe78960"
   license "GPL-2.0-or-later"
 
   livecheck do
