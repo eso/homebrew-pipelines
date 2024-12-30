eae8a685fd5e6c5644ecfc3feecf31cf6179fb81
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-30 13:40:41 +0100

    added vimos recipe

---
+++ Formula/esopipe-vimos-recipes.rb
@@ -0,0 +1,60 @@
+class EsopipeVimosRecipes < Formula
+  desc "ESO VIMOS instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/vimos/vimos-kit-4.1.10.tar.gz"
+  sha256 "e4394926ada4e5f59be3de67d56630bf113943d2398298a8f2b7abf1e908de4e"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?vimos-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  def name_version
+    "vimos-#{version.major_minor_patch}"
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "cfitsio@4.2.0"
+  depends_on "cpl@7.3.2"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl@2.6"
+
+  uses_from_macos "curl"
+
+  def install
+    system "tar", "xf", "#{name_version}.tar.gz"
+    cd name_version.to_s do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cfitsio=#{Formula["cfitsio@4.2.0"].prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}",
+                            "--with-gsl=#{Formula["gsl@2.6"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  def post_install
+    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
+    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
+      ohai "Updating #{workflow}"
+      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
+      end
+      if workflow.read.include?("$ROOT_DATA_DIR/reflex_input")
+        inreplace workflow, "$ROOT_DATA_DIR/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
+      end
+      cp workflow, workflow_dir_2
+    end
+  end
+
+  test do
+    assert_match "vimos_ima_dark -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page vimos_ima_dark")
+  end
+end
