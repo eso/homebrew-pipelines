f443b7e31f077a4c2a973a36acc067c51a2aee20
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-30 13:11:07 +0100

    added midi recipe

---
+++ Formula/esopipe-midi-recipes.rb
@@ -0,0 +1,55 @@
+class EsopipeMidiRecipes < Formula
+  desc "ESO MIDI instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/midi/midi-kit-2.9.6-1.tar.gz"
+  sha256 "f57b483a118fda01335723f51e2eca4c795fa64d3b16dcdcd236b297ea2159d7"
+  license "GPL-2.0-or-later"
+
+  def name_version
+    "midi-#{version.major_minor_patch}"
+  end
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?midi-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "cfitsio@4.2.0"
+  depends_on "cpl@7.3.2"
+  depends_on "esorex"
+
+  uses_from_macos "curl"
+
+  def install
+    system "tar", "xf", "#{name_version}.tar.gz"
+    cd name_version.to_s do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cfitsio=#{Formula["cfitsio@4.2.0"].prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  def post_install
+    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
+    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
+      ohai "Updating #{workflow}"
+      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE/reflex_input")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
+      end
+      cp workflow, workflow_dir_2
+    end
+  end
+
+  test do
+    assert_match "midi_profile -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page midi_profile")
+  end
+end
