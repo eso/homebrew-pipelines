e2f4d4e2f58425288dcfc4bea0a51420d551e9c8
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-30 13:39:18 +0100

    added vcam recipe
    Closes #88.
    
    Signed-off-by: Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>

---
+++ Formula/esopipe-vcam-recipes.rb
@@ -0,0 +1,51 @@
+class EsopipeVcamRecipes < Formula
+  desc "ESO VIRCAM instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/vircam/vcam-kit-2.3.14.tar.gz"
+  sha256 "d99dc912322f757997b9adf731feeb25b515220500f3efd46deed88ead34f176"
+  license "GPL-2.0-or-later"
+
+  def name_version
+    "vcam-#{version.major_minor_patch}"
+  end
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?vcam-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "cpl@7.3.2"
+  depends_on "esorex"
+
+  def install
+    system "tar", "xf", "#{name_version}.tar.gz"
+    cd name_version.to_s do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  def post_install
+    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
+    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
+      ohai "Updating #{workflow}"
+      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
+      end
+      if workflow.read.include?("$ROOT_DATA_DIR/reflex_input")
+        inreplace workflow, "$ROOT_DATA_DIR/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
+      end
+      cp workflow, workflow_dir_2
+    end
+  end
+
+  test do
+    assert_match "vircam_dark_combine -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page vircam_dark_combine")
+  end
+end
