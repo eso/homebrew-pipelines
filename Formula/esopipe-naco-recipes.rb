d046fb07d519b24e24ee0431d547d44165f5cc16
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-30 13:13:02 +0100

    added naco recipe
    Closes #85.
    
    Signed-off-by: Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>

---
+++ Formula/esopipe-naco-recipes.rb
@@ -0,0 +1,58 @@
+class EsopipeNacoRecipes < Formula
+  desc "ESO NACO instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/naco/naco-kit-4.4.13-1.tar.gz"
+  sha256 "999ed3bbd574f0821e0c00d8d51e41aff14c9ebf4cea586c642b8da5e048e383"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?naco-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  def name_version
+    "naco-#{version.major_minor_patch}"
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "cpl@7.3.2"
+  depends_on "erfa"
+  depends_on "esorex"
+  depends_on "gsl@2.6"
+
+  uses_from_macos "curl"
+
+  def install
+    system "tar", "xf", "#{name_version}.tar.gz"
+    cd name_version.to_s do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+                            "--with-erfa=#{Formula["erfa"].prefix}",
+                            "--with-curl=#{Formula["curl"].prefix}",
+                            "--with-gsl=#{Formula["gsl@2.6"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  def post_install
+    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
+    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
+      ohai "Updating #{workflow}"
+      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
+      end
+      if workflow.read.include?("$ROOT_DATA_DIR/reflex_input")
+        inreplace workflow, "$ROOT_DATA_DIR/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
+      end
+      cp workflow, workflow_dir_2
+    end
+  end
+
+  test do
+    assert_match "naco_img_dark -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page naco_img_dark")
+  end
+end
