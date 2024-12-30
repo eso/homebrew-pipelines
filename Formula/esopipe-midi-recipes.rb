cce43ce4fbf8bf60e581ebe8893c0e0f97de3d92
Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>
2024-12-30 12:22:24 +0000

    esopipe-midi-recipes: add 2.9.6-1 bottle.

--- Formula/esopipe-midi-recipes.rb
+++ Formula/esopipe-midi-recipes.rb
@@ -5,6 +5,14 @@ class EsopipeMidiRecipes < Formula
   sha256 "f57b483a118fda01335723f51e2eca4c795fa64d3b16dcdcd236b297ea2159d7"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-midi-recipes-2.9.6-1"
+    sha256 arm64_sequoia: "3c2c946cb228001ef6d9b9aae8dfc8b2bda7c4ea2196ab3349a8102f7d874bfd"
+    sha256 arm64_sonoma:  "324ca5c061e6e3c7ae9f4caa5ce4bc82c9180b9467d6f1a94b59e5b2bdff2e8b"
+    sha256 ventura:       "ff3cbf9c3f64e1a60eb514125b8c089fba4ea7d7772ea7b3a471e6226b284038"
+    sha256 x86_64_linux:  "4ab8b4b0a0924e17fe159ff3aae7c23a40e43860db701553e27176f03ea01f82"
+  end
+
   def name_version
     "midi-#{version.major_minor_patch}"
   end
