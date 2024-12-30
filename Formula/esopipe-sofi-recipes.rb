d20ac1d92cf467102ca533105f35112cf08ba4d7
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-30 13:16:28 +0100

    added sofi recipe

---
+++ Formula/esopipe-sofi-recipes.rb
@@ -0,0 +1,53 @@
+class EsopipeSofiRecipes < Formula
+  desc "ESO SOFI instrument pipeline (recipe plugins)"
+  homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sofi/sofi-kit-1.5.16.tar.gz"
+  sha256 "ce0fc266650c962291f0ce07fc32b687703fed6c6440a68114022d47c96b165e"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?sofi-kit-(\d+(?:[.-]\d+)+)\.t/i)
+  end
+
+  def name_version
+    "sofi-#{version.major_minor_patch}"
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "cpl@7.3.2"
+  depends_on "esorex"
+  depends_on "gsl@2.6"
+
+  def install
+    system "tar", "xf", "#{name_version}.tar.gz"
+    cd name_version.to_s do
+      system "./configure", "--prefix=#{prefix}",
+                            "--with-cpl=#{Formula["cpl@7.3.2"].prefix}",
+                            "--with-gsl=#{Formula["gsl@2.6"].prefix}"
+      system "make", "install"
+    end
+  end
+
+  def post_install
+    workflow_dir_1 = prefix/"share/reflex/workflows/#{name_version}"
+    workflow_dir_2 = prefix/"share/esopipes/#{name_version}/reflex"
+    workflow_dir_1.glob("*.xml").each do |workflow|
+      ohai "Updating #{workflow}"
+      if workflow.read.include?("CALIB_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "CALIB_DATA_PATH_TO_REPLACE", HOMEBREW_PREFIX/"share/esopipes/datastatic"
+      end
+      if workflow.read.include?("ROOT_DATA_PATH_TO_REPLACE")
+        inreplace workflow, "ROOT_DATA_PATH_TO_REPLACE", "#{Dir.home}/reflex_data"
+      end
+      if workflow.read.include?("$ROOT_DATA_DIR/reflex_input")
+        inreplace workflow, "$ROOT_DATA_DIR/reflex_input", HOMEBREW_PREFIX/"share/esopipes/datademo"
+      end
+      cp workflow, workflow_dir_2
+    end
+  end
+
+  test do
+    assert_match "sofi_img_dark -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page sofi_img_dark")
+  end
+end
