96f02b0b99b10d1675420a48dcc0fcaf47cbda52
szampier <56970291+szampier@users.noreply.github.com>
2025-01-24 17:30:28 +0100

    add cfitsio@4.5.0.rb
    Closes #97.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

---
+++ Formula/cfitsio@4.5.0.rb
@@ -0,0 +1,39 @@
+class CfitsioAT450 < Formula
+  desc "C access to FITS data files with optional Fortran wrappers"
+  homepage "https://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html"
+  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.5.0.tar.gz"
+  sha256 "e4854fc3365c1462e493aa586bfaa2f3d0bb8c20b75a524955db64c27427ce09"
+  license "CFITSIO"
+  revision 1
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?cfitsio[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "cmake" => :build
+  uses_from_macos "zlib"
+
+  # Fix pkg-config file location, should be removed on next release
+  patch do
+    url "https://github.com/HEASARC/cfitsio/commit/d2828ae5af42056bb4fde397f3205479d01a4cf1.patch?full_index=1"
+    sha256 "690d0bde53fc276f53b9a3f5d678ca1d03280fae7cfa84e7b59b87304fcdcb46"
+  end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    (pkgshare/"testprog").install Dir["testprog*", "utilities/testprog.c"]
+  end
+
+  test do
+    cp Dir["#{pkgshare}/testprog/testprog*"], testpath
+    system ENV.cc, "testprog.c", "-o", "testprog", "-I#{include}",
+                   "-L#{lib}", "-lcfitsio"
+    system "./testprog > testprog.lis"
+    cmp "testprog.lis", "testprog.out"
+    cmp "testprog.fit", "testprog.std"
+  end
+end
