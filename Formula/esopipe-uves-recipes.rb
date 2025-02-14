f9039cdb4244331df0237778893b6f1c8c0bdbd4
Stefano Zampieri <szampier@eso.org>
2025-02-15 00:31:40 +0100

    esopipe-uves-recipes: revision bump

--- Formula/esopipe-uves-recipes.rb
+++ Formula/esopipe-uves-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeUvesRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/uves/uves-kit-6.4.10.tar.gz"
   sha256 "57fc759cec5d42a146ace61c6b8b423144ed0d5968b10dafaa45b5612fe78960"
   license "GPL-2.0-or-later"
+  revision 1
 
   def name_version
     "uves-#{version.major_minor_patch}"
