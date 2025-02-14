5184b13d3de75b1511ba8677eb808b1f7a7b5600
Stefano Zampieri <szampier@eso.org>
2025-02-15 00:30:08 +0100

    esopipe-matisse-recipes: revision bump
    Closes #124.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-matisse-recipes.rb
+++ Formula/esopipe-matisse-recipes.rb
@@ -4,7 +4,7 @@ class EsopipeMatisseRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/matisse/matisse-kit-2.0.2-1.tar.gz"
   sha256 "44bb7aeba1b5f91589f78594030fec16a63d04f44db9d08e9253cb3d04288b5b"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   def name_version
     "matisse-#{version.major_minor_patch}"
