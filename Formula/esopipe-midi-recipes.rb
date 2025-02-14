a15578b8dc465f28748eceecc96ce806263962c5
Stefano Zampieri <szampier@eso.org>
2025-02-15 00:30:18 +0100

    esopipe-midi-recipes: revision bump
    Closes #125.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-midi-recipes.rb
+++ Formula/esopipe-midi-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeMidiRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/midi/midi-kit-2.9.6-1.tar.gz"
   sha256 "f57b483a118fda01335723f51e2eca4c795fa64d3b16dcdcd236b297ea2159d7"
   license "GPL-2.0-or-later"
+  revision 1
 
   def name_version
     "midi-#{version.major_minor_patch}"
