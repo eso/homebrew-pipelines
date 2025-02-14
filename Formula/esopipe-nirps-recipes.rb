59a0747a493c05a1a8e961fa6211f4278bf44fb9
Stefano Zampieri <szampier@eso.org>
2025-02-15 00:30:59 +0100

    esopipe-nirps-recipes: revision bump

--- Formula/esopipe-nirps-recipes.rb
+++ Formula/esopipe-nirps-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeNirpsRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/nirps/nirps-kit-3.3.0.tar.gz"
   sha256 "efb541607aadfb22cbba8cfc5c581c8c4a48ae6ea8b8b92080eb36b615b80e51"
   license "GPL-2.0-or-later"
+  revision 1
 
   def name_version
     "nirps-#{version.major_minor_patch}"
