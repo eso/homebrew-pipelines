b027282143476b4dd8e7fc65589727adcec5f0b3
Stefano Zampieri <szampier@eso.org>
2025-02-15 00:28:24 +0100

    esopipe-espda-recipes: revision bump
    Closes #114.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-espda-recipes.rb
+++ Formula/esopipe-espda-recipes.rb
@@ -4,7 +4,7 @@ class EsopipeEspdaRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso-das/espda-kit-1.3.8.tar.gz"
   sha256 "d100d9c4318fcfd853f445413ee5a73ad8ca5f41f359ea016afae3758e7e33d6"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   def name_version
     "espda-#{version.major_minor_patch}"
