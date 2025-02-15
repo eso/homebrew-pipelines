abeea538638125b3d61a12a018f33a3a348d1dca
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-02-15 08:50:15 +0000

    esopipe-nirps-recipes: add 3.3.0_1 bottle.

--- Formula/esopipe-nirps-recipes.rb
+++ Formula/esopipe-nirps-recipes.rb
@@ -6,6 +6,14 @@ class EsopipeNirpsRecipes < Formula
   license "GPL-2.0-or-later"
   revision 1
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-nirps-recipes-3.3.0_1"
+    sha256 arm64_sequoia: "001703c821b8aaa2a59b11a07dcfdc072440a41a5551d1bac9a8a64b836118dd"
+    sha256 arm64_sonoma:  "1d330b7cc8569fdf4bbb8ed2984a62ae9f58124ba21e87de8d9d36c3ca2c846e"
+    sha256 ventura:       "9cd4d234239f7ef8822adfc53e6c8685ad865c985d000a56bde98fb9a2798008"
+    sha256 x86_64_linux:  "a3024273069620df96624fca9fc30403f567e0d62471684d7a409f6d68cd6778"
+  end
+
   def name_version
     "nirps-#{version.major_minor_patch}"
   end
