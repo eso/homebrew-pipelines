d7f595efb32ee6bb439fa0322a04768a9d0f4005
Stefano Zampieri <szampier@eso.org>
2025-02-28 15:52:51 +0100

    esopipe-espdr-recipes 3.3.0-4

--- Formula/esopipe-espdr-recipes.rb
+++ Formula/esopipe-espdr-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeEspdrRecipes < Formula
   desc "ESO ESPRESSO instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso/espdr-kit-3.3.0.tar.gz"
-  sha256 "4ec59051a56b895c6d2be921eff30c7532ed7d05ab9fb217cd88abf9941387c4"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso/espdr-kit-3.3.0-4.tar.gz"
+  sha256 "72a3b6fd4edcb1f7fe00f4188bb228ff438e92789ec090b63b7bfc5a02ebb3b9"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :homepage
