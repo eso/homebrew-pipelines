40bcf7a2129ecc554f9d43247be4702081b0d12b
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-02-28 16:44:07 +0000

    esopipe-uves-recipes: update 6.4.10-4 bottle.

--- Formula/esopipe-uves-recipes.rb
+++ Formula/esopipe-uves-recipes.rb
@@ -11,11 +11,11 @@ class EsopipeUvesRecipes < Formula
   end
 
   bottle do
-    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-uves-recipes-6.4.10_1"
-    sha256 cellar: :any,                 arm64_sequoia: "0a79b58d62b6eec5185560802d6887ee81789c55a3f09f38232e71ba060c7778"
-    sha256 cellar: :any,                 arm64_sonoma:  "84656a64512240d34edd8d2dc4bd21b304fb760fc402961324ec7e10453296d2"
-    sha256 cellar: :any,                 ventura:       "a9f983fecbd7c5104afe3db9a3320b779405e61f752b81a571aab511268d59af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c77e5fbd06af91b3a29fbdbbff184e5230c0b8b049f1da89f9de3435d413679"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-uves-recipes-6.4.10-4"
+    sha256 cellar: :any,                 arm64_sequoia: "201118ce9235e7ff6b8e0a341a6309e950e7851db1a78d9df80164f73516e9a0"
+    sha256 cellar: :any,                 arm64_sonoma:  "fe6e5884d0b9046311b1ca65b72fc374f89b6ca929a74a7096551011d1439d72"
+    sha256 cellar: :any,                 ventura:       "7fff869b7613b6edb99a1523a24c424e561177b0853b4c37be488abbb1f5bdfb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d9a99c17b48e336da86ab8b571e14292ae3bd7ff61fc056eeeaecdee7d8003c"
   end
 
   def name_version
