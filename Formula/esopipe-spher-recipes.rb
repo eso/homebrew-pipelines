26d829e9f9905af1d3d9bb2ae840b8b07e35b0df
Stefano Zampieri <szampier@eso.org>
2025-02-28 16:15:18 +0100

    esopipe-spher-recipes 0.56.0-6
    Closes #163.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-spher-recipes.rb
+++ Formula/esopipe-spher-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeSpherRecipes < Formula
   desc "ESO SPHERE instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.56.0.tar.gz"
-  sha256 "0c06cfa743b85fbf2980d0dbdb1bcf8cf57ea591e1f1cec987d6ca4abdc25157"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.56.0-6.tar.gz"
+  sha256 "f99105178a212cb28492612d466d1973a6d0c45a278ed14384b13296f818b1f1"
   license "GPL-2.0-or-later"
-  revision 3
 
   livecheck do
     url :homepage
