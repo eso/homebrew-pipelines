0488a4b293c091e240dfa25da5bd59d771489f59
Stefano Zampieri <szampier@eso.org>
2025-02-28 15:34:56 +0100

    esopipe-detmon-recipes 1.3.14-4
    Closes #142.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-detmon-recipes.rb
+++ Formula/esopipe-detmon-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeDetmonRecipes < Formula
   desc "ESO DETMON instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/detmon/detmon-kit-1.3.14.tar.gz"
-  sha256 "4d7ea0eb8e082d741ebd074c53165d2b7b1868582bde57ab715833efd17f69f3"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/detmon/detmon-kit-1.3.14-4.tar.gz"
+  sha256 "35d1b37cd349bb786bf46fbf1e1f51dce90549ac5ecdcf8a6362e34798d9bf6a"
   license "GPL-2.0-or-later"
-  revision 4
 
   livecheck do
     url :homepage
