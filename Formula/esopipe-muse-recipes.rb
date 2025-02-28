2629d18436316e0a59c01f0b819435c20c68b546
Stefano Zampieri <szampier@eso.org>
2025-02-28 16:08:23 +0100

    esopipe-muse-recipes 2.10.10-4
    Closes #158.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-muse-recipes.rb
+++ Formula/esopipe-muse-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeMuseRecipes < Formula
   desc "ESO MUSE instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.10.10.tar.gz"
-  sha256 "d3d51e755a0fdffc21d5730e0041f4e3a4826ec9ffcb97c1f0f69860a7a693df"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.10.10-4.tar.gz"
+  sha256 "4e58bacf417af7bf89f4e6743c637f604406592e35153628112bf369696fb9fa"
   license "GPL-2.0-or-later"
-  revision 4
 
   livecheck do
     url :homepage
