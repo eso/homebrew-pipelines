9a736a357c7d5cc096bd9ccebd9f1c3adcb52fd9
Stefano Zampieri <szampier@eso.org>
2025-02-28 15:35:22 +0100

    esopipe-efosc-recipes 2.3.9-13
    Closes #143.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-efosc-recipes.rb
+++ Formula/esopipe-efosc-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeEfoscRecipes < Formula
   desc "ESO EFOSC instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/efosc/efosc-kit-2.3.9-2.tar.gz"
-  sha256 "515a11ddaa6f71d2ebcfff91433b782089f93af5fb7ce3daf973ccfb456f9bba"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/efosc/efosc-kit-2.3.9-13.tar.gz"
+  sha256 "478a32007be3dabbfb2f457807c31db3c57531510c1174aa9085d96809d7e784"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :homepage
