2e489b8578f1166455ca3b6d6282216740fa00a6
szampier <56970291+szampier@users.noreply.github.com>
2025-03-04 17:16:15 +0100

    update url and sha256 of datastatic formulae

--- Formula/esopipe-molecfit.rb
+++ Formula/esopipe-molecfit.rb
@@ -1,8 +1,8 @@
 class EsopipeMolecfit < Formula
   desc "ESO MOLECFIT instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/molecfit/molecfit-kit-4.3.3-5.tar.gz"
-  sha256 "749b5b31c7f6c29c7904da8145fbacebe3add0ab7d6ef8cf9d3b428df20c5dd0"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/molecfit/molecfit-kit-4.3.3-6.tar.gz"
+  sha256 "cbb6eea020fb5e350afd3f506c079e8632ee94c531859eee7e57dedf8b4705f4"
   license "GPL-2.0-or-later"
 
   livecheck do
