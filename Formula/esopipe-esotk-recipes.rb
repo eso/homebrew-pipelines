8f4f59ecb2d0f7eb5735d4ce4f2fa2b690a6b666
Stefano Zampieri <szampier@eso.org>
2025-03-13 20:25:38 +0100

    esopipe-esotk-recipes 0.9.8-10

--- Formula/esopipe-esotk-recipes.rb
+++ Formula/esopipe-esotk-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeEsotkRecipes < Formula
   desc "ESO ESOTK instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/esotk/esotk-kit-0.9.8-9.tar.gz"
-  sha256 "033d0da065828c26863d8b653058c33fe8ae7ae5c27a7920f7bcf09f82652116"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/esotk/esotk-kit-0.9.8-10.tar.gz"
+  sha256 "cc2c578a96f88ff7e287a13d6a8d1e4ecb05c545c80c973fb95889135cfea942"
   license "GPL-2.0-or-later"
 
   livecheck do
