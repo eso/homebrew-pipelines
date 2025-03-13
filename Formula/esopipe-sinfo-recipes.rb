4990f8afdb52a63f90438248837d5e2ee7c53616
Stefano Zampieri <szampier@eso.org>
2025-03-13 20:37:30 +0100

    esopipe-sinfo-recipes 3.3.6-6
    Closes #197.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-sinfo-recipes.rb
+++ Formula/esopipe-sinfo-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeSinfoRecipes < Formula
   desc "ESO SINFONI instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sinfoni/sinfo-kit-3.3.6-5.tar.gz"
-  sha256 "d5f170deaf5787a371056cfe12d825f7a67db3a7337c4e025dd5e4fa13cf0432"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sinfoni/sinfo-kit-3.3.6-6.tar.gz"
+  sha256 "7f4f4a652d2c0e83091425f9bf27b1f1d2f76b88590f8704626b24a2dd6ef093"
   license "GPL-2.0-or-later"
 
   livecheck do
