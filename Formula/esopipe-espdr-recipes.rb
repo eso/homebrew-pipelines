0b5fde265e3e5492d2a5f7321519edf474c921fc
szampier <56970291+szampier@users.noreply.github.com>
2025-03-18 13:43:04 +0100

    esopipe-espdr-recipes 3.3.9

--- Formula/esopipe-espdr-recipes.rb
+++ Formula/esopipe-espdr-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeEspdrRecipes < Formula
   desc "ESO ESPRESSO instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso/espdr-kit-3.3.6.tar.gz"
-  sha256 "4172f3a81313af86abf2684d4621d97946ba87428d7cc5394cbdd54214e4a983"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso/espdr-kit-3.3.9.tar.gz"
+  sha256 "74115340977896ad17d330f4c4f240ff5461d00fac22691c89641a53ebe97415"
   license "GPL-2.0-or-later"
 
   livecheck do
