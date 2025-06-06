f34353dda9f25b2987723ee8041faa2ee413875f
szampier <56970291+szampier@users.noreply.github.com>
2025-06-06 15:56:05 +0200

    bump version of esopipe-vimos.rb

--- Formula/esopipe-vimos.rb
+++ Formula/esopipe-vimos.rb
@@ -1,8 +1,8 @@
 class EsopipeVimos < Formula
   desc "ESO VIMOS instrument pipeline (data static)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/vimos/vimos-kit-4.1.12-6.tar.gz"
-  sha256 "0f78a74443d7f6b956f050cad78301e8e8db7a11ab7dfc8610668d4de65e5e3e"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/vimos/vimos-kit-4.1.13-1.tar.gz"
+  sha256 "bdf66caf2f61eb710d5d8466f50df8caae24f5e1c71bf9e19e22d9bc9e5cadea"
   license "GPL-2.0-or-later"
 
   livecheck do
