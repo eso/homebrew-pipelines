04e5c8d0566307da50fe96214641c2658f7581e4
Stefano Zampieri <szampier@eso.org>
2025-06-13 10:54:42 +0200

    esopipe-crires-recipes: revision bump
    Closes #224.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-crires-recipes.rb
+++ Formula/esopipe-crires-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeCriresRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/crires/crire-kit-2.3.19-6.tar.gz"
   sha256 "48b996d430a528ab6515d5022e26c2a98b5235d4f0a1b1f84f04d47f19918fa3"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
