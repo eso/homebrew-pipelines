76ba86c89dd82b74c8ff2057e6681d33d883d584
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-06-14 11:17:40 +0000

    esopipe-matisse-recipes: add 2.2.0-5_2 bottle.

--- Formula/esopipe-matisse-recipes.rb
+++ Formula/esopipe-matisse-recipes.rb
@@ -11,6 +11,14 @@ class EsopipeMatisseRecipes < Formula
     regex(/href=.*?matisse-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-matisse-recipes-2.2.0-5_2"
+    sha256 cellar: :any,                 arm64_sequoia: "4f9df127ee02d6aabdebe8135b6b7f54a41e7235d98c9aa0e6c80dcb7069de80"
+    sha256 cellar: :any,                 arm64_sonoma:  "6b47ce136b98be0ec96f2e5846c81bd828d934e4c0a2da73ac248a808f8d9d33"
+    sha256 cellar: :any,                 ventura:       "bd11aa561ce2b9b13b0199247ef18c0163bd7bf88649411c11050a58d74bff86"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3c53de95342016fb030fa9b982ffe887d32603acb1c39814ac788049b8c2cfe5"
+  end
+
   def name_version
     "matisse-#{version.major_minor_patch}"
   end
