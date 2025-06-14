fd2d3416bd80dff756671d1eed13d1410561fe18
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-06-14 15:25:55 +0000

    pyesorex: add 1.0.3_1 bottle.

--- Formula/pyesorex.rb
+++ Formula/pyesorex.rb
@@ -13,6 +13,14 @@ class Pyesorex < Formula
     regex(/href=.*?pyesorex[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/pyesorex-1.0.3_1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "66a6349e0e97eab83dffce0fcb8001339f644f04695a8ce55022a630fb1bd21c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "16d2ac6cf990dc7796fe9a3ded06de5dc70de60b75c2aa5117b5fb5a40dee418"
+    sha256 cellar: :any_skip_relocation, ventura:       "aad2dd35882726df272c863689a6dc082444c9c3d71ca043b43b1d6f5947ba23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "27b2e03f9bfcb57b6e7f8524b91bea05f68545f4781b5ce4e7bfb648fea98134"
+  end
+
   depends_on "pycpl"
   depends_on "python@3.11"
 
