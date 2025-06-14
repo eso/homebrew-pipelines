97927c1d26ea4ed2c13df9e85efbf0b6cb5163c8
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-06-14 11:14:53 +0000

    esopipe-crires-recipes: add 2.3.19-6_2 bottle.

--- Formula/esopipe-crires-recipes.rb
+++ Formula/esopipe-crires-recipes.rb
@@ -11,6 +11,14 @@ class EsopipeCriresRecipes < Formula
     regex(/href=.*?crire-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-crires-recipes-2.3.19-6_2"
+    sha256 cellar: :any,                 arm64_sequoia: "0ef3623118718117529f854c8342c5857a02248435e77e14cc4c59cffc10ca66"
+    sha256 cellar: :any,                 arm64_sonoma:  "5c966230d2a6ada8a31fd55fce5aa9590b5eff461b68a1468a41d15eba7fff13"
+    sha256 cellar: :any,                 ventura:       "34fcebcbb0f6f462cc65e78430c3c6684b486659a4c7024eb0070934ca45ac83"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc6dc886c990df00a5a25aeb31cd9a3b804d809deffc2e6d4d9ea2898b44715c"
+  end
+
   def name_version
     "crire-#{version.major_minor_patch}"
   end
