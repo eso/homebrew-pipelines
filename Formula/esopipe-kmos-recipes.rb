9b560503826b2621b5ace004c66a0849ce479da4
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-06-14 11:17:30 +0000

    esopipe-kmos-recipes: add 4.5.1_2 bottle.

--- Formula/esopipe-kmos-recipes.rb
+++ Formula/esopipe-kmos-recipes.rb
@@ -11,6 +11,14 @@ class EsopipeKmosRecipes < Formula
     regex(/href=.*?kmos-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-kmos-recipes-4.5.1_2"
+    sha256 cellar: :any,                 arm64_sequoia: "9984c6f5776ff31804e3ecafb647cd25fa3ba876262d72cc87d927c5b8682b3d"
+    sha256 cellar: :any,                 arm64_sonoma:  "f0dbf4d4e38267c114f9c0b40a0690e5c3c04ba1b0f81df386a4a3d6f526920e"
+    sha256 cellar: :any,                 ventura:       "64e823128438c629f1f1d25c2c64246d8fb2149d58ecd33d833aa9b911606ce5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ecb2367131644f3c113321dd03409c53e86a2fb86ec0d4f8eafd6fbaf773852"
+  end
+
   def name_version
     "kmos-#{version.major_minor_patch}"
   end
