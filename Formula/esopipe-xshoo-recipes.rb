0036f276b4c418ef12433cdd11287de5b46cca33
szampier <56970291+szampier@users.noreply.github.com>
2025-06-17 17:37:28 +0200

    esopipe-xshoo-recipes 3.8.1-2
    Closes #339.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-xshoo-recipes.rb
+++ Formula/esopipe-xshoo-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeXshooRecipes < Formula
   desc "ESO XSHOOTER instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/xshooter/xshoo-kit-3.8.1.tar.gz"
-  sha256 "ca470d7c512861c5f43c8328a6054541047764876ded828258db26566746e003"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/xshooter/xshoo-kit-3.8.1-2.tar.gz"
+  sha256 "93c21ee1a95afdf387df1369e8226df0e0066e74c6b4a43d125531ffb8023ca7"
   license "GPL-2.0-or-later"
-  revision 2
 
   livecheck do
     url :homepage
