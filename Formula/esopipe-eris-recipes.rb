6d0ec66d52f517a41de3aef4ee28519d93d314ee
szampier <56970291+szampier@users.noreply.github.com>
2025-06-17 17:28:36 +0200

    esopipe-eris-recipes 1.8.1-1

--- Formula/esopipe-eris-recipes.rb
+++ Formula/esopipe-eris-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeErisRecipes < Formula
   desc "ESO ERIS instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/eris/eris-kit-1.8.1.tar.gz"
-  sha256 "56958bdebd439223699ce0cdfe27f18ce9d73e4eb2bd9748347eef92de193318"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/eris/eris-kit-1.8.1-1.tar.gz"
+  sha256 "132c2ccad257b64777f2ee026ac04abc603da84c6e4a04f2ad94e4465e216e73"
   license "GPL-2.0-or-later"
-  revision 2
 
   livecheck do
     url :homepage
