44f3f5fea699359448474c47dc4b972e22e1a728
szampier <56970291+szampier@users.noreply.github.com>
2025-06-17 17:27:03 +0200

    esopipe-amber-recipes 4.4.5-9
    Closes #309.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-amber-recipes.rb
+++ Formula/esopipe-amber-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeAmberRecipes < Formula
   desc "ESO AMBER instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/amber/amber-kit-4.4.5-8.tar.gz"
-  sha256 "b2a8576ba2fc8ca620b39db7f4ca652375276a10a984183668e5d8479e0778c5"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/amber/amber-kit-4.4.5-9.tar.gz"
+  sha256 "7b4339232d97267b23aa09e7718599edfeec273cb7f1a3f7821888a217946a4f"
   license "GPL-2.0-or-later"
-  revision 2
 
   livecheck do
     url :homepage
