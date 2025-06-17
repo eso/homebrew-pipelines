1ace713558392b3370632efe17fc41f7a00777b3
szampier <56970291+szampier@users.noreply.github.com>
2025-06-17 17:32:31 +0200

    esopipe-kmos-recipes 4.5.1-2

--- Formula/esopipe-kmos-recipes.rb
+++ Formula/esopipe-kmos-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeKmosRecipes < Formula
   desc "ESO KMOS instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/kmos/kmos-kit-4.5.1.tar.gz"
-  sha256 "da1c7c61454e2f3c0ac858aaf66a40ff9c3124a1634eef4906bbb8d8365746c2"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/kmos/kmos-kit-4.5.1-2.tar.gz"
+  sha256 "f7e7d05611a39d4193e486abb88db8856f1705d075af2048b557757f6572879f"
   license "GPL-2.0-or-later"
-  revision 2
 
   livecheck do
     url :homepage
