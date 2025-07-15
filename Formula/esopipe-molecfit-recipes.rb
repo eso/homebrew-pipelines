38091b595b4fde51c6359d0e33a216e94ab9c961
szampier <56970291+szampier@users.noreply.github.com>
2025-07-15 14:25:00 +0200

    esopipe-molecfit-recipes 4.4.2-5

--- Formula/esopipe-molecfit-recipes.rb
+++ Formula/esopipe-molecfit-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeMolecfitRecipes < Formula
   desc "ESO MOLECFIT instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/molecfit/molecfit-kit-4.4.2-3.tar.gz"
-  sha256 "213dab147500b185a640a52364f9d4d602574062fa2c2f62e7141b09d46284a3"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/molecfit/molecfit-kit-4.4.2-5.tar.gz"
+  sha256 "6a74b04c613cfe221cc0824659eb86ec6d17afbe78fcd1261e2efaa45392480b"
   license "GPL-2.0-or-later"
 
   livecheck do
