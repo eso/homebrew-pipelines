e859a56cdd8cd6f28a265ccbb7cda86c6c769aef
Stefano Zampieri <szampier@eso.org>
2025-08-01 19:26:29 +0200

    esopipe-vimos-recipes 4.1.13-3
    Closes #387.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-vimos-recipes.rb
+++ Formula/esopipe-vimos-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeVimosRecipes < Formula
   desc "ESO VIMOS instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/vimos/vimos-kit-4.1.13-2.tar.gz"
-  sha256 "44beddeac65deefb75921817aa1b3987b4482e27d49f9e6c18138a90d01354e3"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/vimos/vimos-kit-4.1.13-3.tar.gz"
+  sha256 "602403ba51f2f9ef2685e65e036d33cb9e0030d17b99a1265347394db587de9d"
   license "GPL-2.0-or-later"
 
   livecheck do
