da3769471a1f6fa4ff49c743a5dafa493369c4ee
Stefano Zampieri <szampier@eso.org>
2025-08-01 19:26:48 +0200

    esopipe-visir-recipes 4.6.2-1
    Closes #388.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-visir-recipes.rb
+++ Formula/esopipe-visir-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeVisirRecipes < Formula
   desc "ESO VISIR instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/visir/visir-kit-4.6.2.tar.gz"
-  sha256 "cd70410d92e9575645a3e4500e707ce6db4d3eef91758c2b27db9464f7e7c237"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/visir/visir-kit-4.6.2-1.tar.gz"
+  sha256 "18c669e78ba5479f583716f41855a8ac757ad78f257968510cdfdc360a555086"
   license "GPL-2.0-or-later"
 
   livecheck do
