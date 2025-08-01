936158e8947f465395a846f4a7cf835532a39274
Stefano Zampieri <szampier@eso.org>
2025-08-01 19:17:34 +0200

    esopipe-gravity-recipes 1.9.4-1
    Closes #370.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-gravity-recipes.rb
+++ Formula/esopipe-gravity-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeGravityRecipes < Formula
   desc "ESO GRAVITY instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.9.4.tar.gz"
-  sha256 "d34c45cc0ab11f676bff30fad7456228265ffbe761aeca45ed000e2e6ec0756d"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.9.4-1.tar.gz"
+  sha256 "86ee4a6cbbc9e5742bac408c13932efcaf50aa41f02a87086a0b817d88060401"
   license "GPL-2.0-or-later"
 
   livecheck do
