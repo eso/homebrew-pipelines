fd12b2073d9d9f241ced379af296a790b2320be2
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-08-02 08:49:34 +0000

    esopipe-fors-recipes: update 5.8.4-1 bottle.

--- Formula/esopipe-fors-recipes.rb
+++ Formula/esopipe-fors-recipes.rb
@@ -11,11 +11,11 @@ class EsopipeForsRecipes < Formula
   end
 
   bottle do
-    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-fors-recipes-5.8.4"
-    sha256 arm64_sequoia: "a9816ff2bda58851ade6e4987680cd81a7a4f608120df17f15e5c2bad2aaefec"
-    sha256 arm64_sonoma:  "8596dc504871c27f4c55d82f00d26f8adb7c69f1d4689ed351cda06152580421"
-    sha256 ventura:       "46e6ddc2a5a98ada1d0c5592ae7970e2fc452acea4eb3902488f981a42d297c7"
-    sha256 x86_64_linux:  "9fb91bc194ba2a4be0fb259dfe8aa46a5146b01199a0bdfaec676a80f5facc05"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-fors-recipes-5.8.4-1"
+    sha256 arm64_sequoia: "3e6fe02fabf80377a03309e895fae2618afb0de6d02c3c47ea950114f05ffb90"
+    sha256 arm64_sonoma:  "6aa6ace246bd03c6d4f02ee4a479cdfa919e2b0cf17eb2c97b7754935a31cd3a"
+    sha256 ventura:       "59eb32d7cfa507e037e986a93e1f900ae83b73624fa70ad76873baab724e3eed"
+    sha256 x86_64_linux:  "7937982710c881e0d071b03eeb959d96aa08ffc7b4296e5e5f38a5f297e09005"
   end
 
   def name_version
