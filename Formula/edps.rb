ff959129d1f903c35c2999aa9a6b9724843350cf
szampier <56970291+szampier@users.noreply.github.com>
2025-09-11 18:07:34 +0200

    edps 1.6.0

--- Formula/edps.rb
+++ Formula/edps.rb
@@ -3,8 +3,8 @@ class Edps < Formula
 
   desc "ESO Data Processing System"
   homepage "https://www.eso.org/sci/software/edps.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/repositories/stable/src/edps/edps-1.5.7.tar.gz"
-  sha256 "4065853e7f3c37d378cb69be532c47ef616ac0023c103b817745b15cea509f24"
+  url "https://ftp.eso.org/pub/dfs/pipelines/repositories/stable/src/edps/edps-1.6.0.tar.gz"
+  sha256 "9c2eecc4c1b622b3741ca2f4215142c22774dea256379eae436a1813ed582f72"
   license "BSD-3-Clause"
 
   livecheck do
