80d631db6b04a341f2dbdc615a506dc85d4aa99a
Stefano Zampieri <szampier@eso.org>
2025-09-12 09:29:10 +0200

    esopipe-esotk-recipes 1.0.0
    Closes #393.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-esotk-recipes.rb
+++ Formula/esopipe-esotk-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeEsotkRecipes < Formula
   desc "ESO ESOTK instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/esotk/esotk-kit-0.9.9-1.tar.gz"
-  sha256 "8c251507eeb65d16d2c83d69b8d67212b26e80d229f67c7397d9d107639f2169"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/esotk/esotk-kit-1.0.0.tar.gz"
+  sha256 "45fed3d008416ec9ba31ccae3b6e53008617b18dc99cca2a4ad7bca145e0c8bd"
   license "GPL-2.0-or-later"
 
   livecheck do
