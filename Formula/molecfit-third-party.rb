0900c9e25914116bceb4db0a0ca4d59f698be050
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-10-07 16:39:33 +0000

    molecfit-third-party: update 1.9.5_1 bottle.

--- Formula/molecfit-third-party.rb
+++ Formula/molecfit-third-party.rb
@@ -12,11 +12,13 @@ class MolecfitThirdParty < Formula
   end
 
   bottle do
-    root_url "https://github.com/eso/homebrew-pipelines/releases/download/molecfit-third-party-1.9.5"
-    sha256 cellar: :any,                 arm64_sequoia: "fc428edf1768fb9f4bd7f80a3a9f03dfb08d5cb43c880c7d59ae054a8ef7269d"
-    sha256 cellar: :any,                 arm64_sonoma:  "07a6287eeccbf7f6b8e8bd52cd77393d101bd1da93f52e814c0ac4c1e632fd92"
-    sha256 cellar: :any,                 ventura:       "10c75f6552fc6effaff5e2ad17a0a5fcfa07a9a5510a81934d4e54c6659eeb6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1f46aacdb106ed0c595fa81e142933e4d3f7f2030c71518d039729bfdaad1cdb"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/molecfit-third-party-1.9.5_1"
+    sha256 cellar: :any,                 arm64_tahoe:   "fd735b8a88d64cb1ca6f052ce4cc7904bad088ad416206f3d1c04b9a9601da02"
+    sha256 cellar: :any,                 arm64_sequoia: "5e9c9a4172d8930737549c643247815d89c8134d913409dd4f08876c743c6854"
+    sha256 cellar: :any,                 arm64_sonoma:  "860716d238a2366f5a769684c5f7a49c4e35e2972ca7735ad0e768325c21f0c0"
+    sha256 cellar: :any,                 sequoia:       "bafbfb6193ebab1f4c0ccb69da9f2a75a9140e1713d1ac13d99fd5c00e88ea85"
+    sha256 cellar: :any,                 sonoma:        "b3469dc1d0d7690ff51874185c2cf87e7d75e09ef37bedd7e0c5b1261a91b126"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9f18315216ded6eed919fad613c465b2f9f5602ad6c3ba406d9852e2e3876c75"
   end
 
   depends_on "gcc"
