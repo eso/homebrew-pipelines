57b80c6e2c5f076ea4eb3c3a7387a7abb112a573
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-10-07 16:39:18 +0000

    erfa: update 2.0.1_4 bottle.

--- Formula/erfa.rb
+++ Formula/erfa.rb
@@ -12,11 +12,13 @@ class Erfa < Formula
   end
 
   bottle do
-    root_url "https://github.com/eso/homebrew-pipelines/releases/download/erfa-2.0.1_3"
-    sha256 cellar: :any,                 arm64_sequoia: "452c76d8e7027a60117c6e27eb65f2db9189e861edded4dd9081d806d039ebc9"
-    sha256 cellar: :any,                 arm64_sonoma:  "4a3769f10590bde883dde6fd61f600bfb4b0e1f511491c0e889bbd191fc02ce4"
-    sha256 cellar: :any,                 ventura:       "49cb84aa66c7b4ed3fad4d34990663049f840f6bec8e20bcbee29fa730065598"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ac0398cb1fd7505e5afc2bf80b5e185fcfac583557a44c7b97ebabe162cf57c"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/erfa-2.0.1_4"
+    sha256 cellar: :any,                 arm64_tahoe:   "1636d406094255692fe573a2e30b58f312020bc5e1073ec38192efd404a1d0f7"
+    sha256 cellar: :any,                 arm64_sequoia: "bc6c56cec36d973deb9fe822f98dc60f0024d51095ea3d0ede1de4d7d1344696"
+    sha256 cellar: :any,                 arm64_sonoma:  "d7775f19cb90590fe1f8756bcf399d6a4de21e3629e2fd8a68469628b0e58678"
+    sha256 cellar: :any,                 sequoia:       "b6f8c9368e9a09ad0c422a453b70388832af5ff034c632b28ad9ce9305103e57"
+    sha256 cellar: :any,                 sonoma:        "6f7b95beb3e1df0d8476dcb3e883c37a8c0e871d6537b973dbd49690f6b77722"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f01f4f228ead4e6f2707e0089101a27079636b9942d2615677d71e2d34104117"
   end
 
   def install
