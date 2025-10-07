8bb8a392ae1c612cf0d1bf0e75daae68959e4038
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2025-10-07 20:00:11 +0000

    telluriccorr: update 4.3.3_4 bottle.

--- Formula/telluriccorr.rb
+++ Formula/telluriccorr.rb
@@ -12,11 +12,12 @@ class Telluriccorr < Formula
   end
 
   bottle do
-    root_url "https://github.com/eso/homebrew-pipelines/releases/download/telluriccorr-4.3.3_3"
-    sha256 arm64_sequoia: "c7366273742f037bac0542a78c9ae2d1e5c8351b6e773cd927133583ca912076"
-    sha256 arm64_sonoma:  "067369cdd6fd0bcd80302a545f8d66b411b71dfb786d6d26af56bc8ee1b907a1"
-    sha256 ventura:       "88532ed8c3e16cda99f6398595afafec2f81019b2a900bcd8a1106d96641f1c0"
-    sha256 x86_64_linux:  "d253d5d9a38fe32b296bf04163f6b22bfdcabfadd7a4d9622da710954e8f164b"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/telluriccorr-4.3.3_4"
+    sha256 arm64_tahoe:   "c4187966c93ec5fc0e0c1b775ec743366d499df1ab7c5dee6abb93fb028cb724"
+    sha256 arm64_sequoia: "062be9738d52947a9e0602bb0cd04fee00027fdc74bd3f8004ce2de75d6ff14a"
+    sha256 arm64_sonoma:  "8b2c3d9a522968df0d2c240940c1a25d87a27bef5b77d1999c8de279eb6c4a14"
+    sha256 sonoma:        "e98109d78c4cc4e925ddf34f8b754658761d4235d8cf152eef995e0609fb768e"
+    sha256 x86_64_linux:  "a19e4d279005cc7777bece392c4774a22e8c5b8cd759c0dff97d08c9ce2c71fd"
   end
 
   depends_on "cpl@7.3.2"
