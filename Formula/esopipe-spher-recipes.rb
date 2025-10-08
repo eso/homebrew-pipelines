8ef2f6101f0f642a0ab10940f719da86269a7a4d
szampier <56970291+szampier@users.noreply.github.com>
2025-10-08 16:21:59 +0200

    esopipe-spher-recipes: revision bump

--- Formula/esopipe-spher-recipes.rb
+++ Formula/esopipe-spher-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeSpherRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.58.1-1.tar.gz"
   sha256 "e158605ccd5df050712c7e77bcb420a8fcd2e7afb99adfc57a244f5d5a7932b7"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
