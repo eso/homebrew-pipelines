144cb5c65cdb634c6577b21239efaee6be26691f
szampier <56970291+szampier@users.noreply.github.com>
2025-10-08 14:06:51 +0200

    esopipe-esotk-recipes: revision bump
    Closes #408.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-esotk-recipes.rb
+++ Formula/esopipe-esotk-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeEsotkRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/esotk/esotk-kit-1.0.0.tar.gz"
   sha256 "45fed3d008416ec9ba31ccae3b6e53008617b18dc99cca2a4ad7bca145e0c8bd"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
