77043b9ea746ae5d9c17bce9713356aea41f8459
szampier <56970291+szampier@users.noreply.github.com>
2025-10-08 14:07:46 +0200

    esopipe-gravity-recipes: revision bump
    Closes #413.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-gravity-recipes.rb
+++ Formula/esopipe-gravity-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeGravityRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.9.6.tar.gz"
   sha256 "a7fe48a9819dde30f3855b0dea98004ea0f736ddea2dc9f6eb0426a79ca3e730"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
