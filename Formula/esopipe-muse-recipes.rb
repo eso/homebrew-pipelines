8429c0d23246b056c667265e0e5ba739c605f152
szampier <56970291+szampier@users.noreply.github.com>
2025-10-08 16:21:20 +0200

    esopipe-muse-recipes: revision bump
    Closes #422.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-muse-recipes.rb
+++ Formula/esopipe-muse-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeMuseRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/muse/muse-kit-2.10.16-1.tar.gz"
   sha256 "4e1aaab34bd8f16833e42d8a895f7b7d8c07faef71bfa52be27564697f3add64"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url :homepage
