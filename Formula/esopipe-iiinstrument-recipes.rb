56af081c12ed86eb5c6adf0358e976c67c6af1fd
szampier <56970291+szampier@users.noreply.github.com>
2025-10-08 16:20:22 +0200

    esopipe-iiinstrument-recipes: revision bump

--- Formula/esopipe-iiinstrument-recipes.rb
+++ Formula/esopipe-iiinstrument-recipes.rb
@@ -4,6 +4,7 @@ class EsopipeIiinstrumentRecipes < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/instruments/iiinstrument/iiinstrument-kit-0.1.16.tar.gz"
   sha256 "58ae45bc8b73234f3097400154cd20c16e1235494c5092c6063ab92d0faee996"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://ftp.eso.org/pub/dfs/pipelines/instruments/iiinstrument/"
