c577feba766b6222eea4defe3fa08b07618e3976
Stefano Zampieri <szampier@eso.org>
2025-10-24 11:42:55 +0300

    esopipe-visir-recipes 4.6.4

--- Formula/esopipe-visir-recipes.rb
+++ Formula/esopipe-visir-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeVisirRecipes < Formula
   desc "ESO VISIR instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/visir/visir-kit-4.6.3.tar.gz"
-  sha256 "15c3b1e7bed4e7c5556df739c063a67b36d5a46009ff1867040fe8c3f502b712"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/visir/visir-kit-4.6.4.tar.gz"
+  sha256 "b0b99638f924c911764fe63b53077449d4d2b336f96def791af1cc96528f53f9"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :homepage
