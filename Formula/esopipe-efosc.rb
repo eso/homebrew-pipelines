7d1d3fb8042c2a4b7dd2c8a6adfa35cb0510910f
szampier <56970291+szampier@users.noreply.github.com>
2026-01-23 18:15:55 +0100

    update esopipe-efosc to version 2.3.12

--- Formula/esopipe-efosc.rb
+++ Formula/esopipe-efosc.rb
@@ -1,8 +1,8 @@
 class EsopipeEfosc < Formula
   desc "ESO EFOSC instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/efosc/efosc-kit-2.3.11-2.tar.gz"
-  sha256 "8c272756f8dab6499782211303e39ed83da228a77f8dc2e457bc4dbbf4f23fb9"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/efosc/efosc-kit-2.3.12.tar.gz"
+  sha256 "8445b4d4dddb5e9fc660910335890c4fcb6da1e9a3d8eed64a8a5b6525454a9e"
   license "GPL-2.0-or-later"
 
   livecheck do
