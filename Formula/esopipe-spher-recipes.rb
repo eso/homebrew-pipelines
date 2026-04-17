8926ef3ac53b6a93d6a22e64fbcae91e150f976f
Stefano Zampieri <szampier@eso.org>
2026-04-17 15:26:29 +0200

    esopipe-spher-recipes 0.59.1

--- Formula/esopipe-spher-recipes.rb
+++ Formula/esopipe-spher-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeSpherRecipes < Formula
   desc "ESO SPHERE instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.58.1-1.tar.gz"
-  sha256 "e158605ccd5df050712c7e77bcb420a8fcd2e7afb99adfc57a244f5d5a7932b7"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sphere/spher-kit-0.59.1.tar.gz"
+  sha256 "999015945f6978930e022af3a7e52f861407cebe54035fd14d639d596067acb5"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :homepage
