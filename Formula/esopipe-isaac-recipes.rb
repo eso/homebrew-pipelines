326cd97e692d2e6ad447a7a76b38983bd44d2c6a
Stefano Zampieri <szampier@eso.org>
2026-04-17 15:18:41 +0200

    esopipe-isaac-recipes 6.2.5-12
    Closes #457.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-isaac-recipes.rb
+++ Formula/esopipe-isaac-recipes.rb
@@ -1,10 +1,9 @@
 class EsopipeIsaacRecipes < Formula
   desc "ESO ISAAC instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/isaac/isaac-kit-6.2.5-9.tar.gz"
-  sha256 "13951e02431e65bd4dfe5951dadfb1c58fedc4d12524d0e3502cd4976cf52405"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/isaac/isaac-kit-6.2.5-12.tar.gz"
+  sha256 "5b622e180a9d833fa08aae31ef221cfe3391953e9e4d48e2e5f713996fc5f45e"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url :homepage
