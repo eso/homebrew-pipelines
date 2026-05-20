be816cf16175eb09451853102911a9cc90f820cc
szampier <56970291+szampier@users.noreply.github.com>
2026-05-20 13:56:12 +0200

    update esopipe-eris to version 2.0.0 with new URL and SHA256 checksum

--- Formula/esopipe-eris.rb
+++ Formula/esopipe-eris.rb
@@ -1,8 +1,8 @@
 class EsopipeEris < Formula
   desc "ESO ERIS instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/eris/eris-kit-1.9.0-1.tar.gz"
-  sha256 "344e283f512167baa2c32e5658b5b6e061fc116aee97b0e5eed4f676a60478b7"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/eris/eris-kit-2.0.0.tar.gz"
+  sha256 "99c973e8b71766bc6d2ea426023f89ebb343fa700dbb62e88624945961db80f3"
   license "GPL-2.0-or-later"
 
   livecheck do
