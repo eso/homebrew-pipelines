1560f9ace0230b03135eb7fd8b95116c55cf1523
szampier <56970291+szampier@users.noreply.github.com>
2026-06-09 14:26:56 +0200

    update esopipe-fors to version 5.8.7 and esopipe-gravity to version 1.10.1 with new URLs and SHA256 checksums

--- Formula/esopipe-gravity.rb
+++ Formula/esopipe-gravity.rb
@@ -1,8 +1,8 @@
 class EsopipeGravity < Formula
   desc "ESO GRAVITY instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.9.6-3.tar.gz"
-  sha256 "64ea2677ef4b2a59c25bfdf8d23ad39e84ebed9399ca978b0003014b9f59cbe3"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.10.1.tar.gz"
+  sha256 "5e8b5a477cdd8071332744ebdfa41ff6bbad51290e6de6b5aa9b8d573146f80f"
   license "GPL-2.0-or-later"
 
   livecheck do
