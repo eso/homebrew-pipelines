76c8c9a4aeaed7cad545ca81c0de558db4569cb7
Stefano Zampieri <szampier@eso.org>
2026-06-26 12:48:23 +0200

    esopipe-gravity-recipes 1.10.2-3

--- Formula/esopipe-gravity-recipes.rb
+++ Formula/esopipe-gravity-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeGravityRecipes < Formula
   desc "ESO GRAVITY instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.10.1.tar.gz"
-  sha256 "5e8b5a477cdd8071332744ebdfa41ff6bbad51290e6de6b5aa9b8d573146f80f"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/gravity/gravity-kit-1.10.2-3.tar.gz"
+  sha256 "6ab6d76f932314e66bd818532e36af6179a96a2ffed4f3e5fbbc740a4f570725"
   license "GPL-2.0-or-later"
 
   livecheck do
