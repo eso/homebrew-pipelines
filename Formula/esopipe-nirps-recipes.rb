035cdf29d7880f32cf3f48d58e236355bcaca1d2
Stefano Zampieri <szampier@eso.org>
2026-06-26 12:57:21 +0200

    esopipe-nirps-recipes 3.3.12-6

--- Formula/esopipe-nirps-recipes.rb
+++ Formula/esopipe-nirps-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeNirpsRecipes < Formula
   desc "ESO NIRPS instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/nirps/nirps-kit-3.3.12-4.tar.gz"
-  sha256 "95b22bab6dc2b010ac549186e89e3cad0dc86d1d65adc424da4023d0638deedc"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/nirps/nirps-kit-3.3.12-6.tar.gz"
+  sha256 "e6882c872bcc3c75242274fc78687e92a82b7d9073db4dae01c3224ffb319be0"
   license "GPL-2.0-or-later"
 
   livecheck do
