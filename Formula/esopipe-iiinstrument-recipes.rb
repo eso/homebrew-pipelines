507c4412b28ea0bedcd521bcb401f6ef24389d4f
Stefano Zampieri <szampier@eso.org>
2026-06-26 12:27:06 +0200

    esopipe-iiinstrument-recipes 0.1.16-6

--- Formula/esopipe-iiinstrument-recipes.rb
+++ Formula/esopipe-iiinstrument-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeIiinstrumentRecipes < Formula
   desc "ESO example template instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/iiinstrument/iiinstrument-kit-0.1.16-3.tar.gz"
-  sha256 "8099289e9e5228289bf3a53280400fb4cb9db42f14048d077296816cd025be82"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/iiinstrument/iiinstrument-kit-0.1.16-6.tar.gz"
+  sha256 "57d62106ad3f26582dce3d6c5467b76a8a75835429fd074786a9cf4dbc7b1806"
   license "GPL-2.0-or-later"
 
   livecheck do
