a972d5e59723c4a1f514fcc1ea3bb1218201d97d
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2026-06-26 14:26:32 +0000

    esopipe-vcam-recipes: add 2.3.17-4 bottle.

--- Formula/esopipe-vcam-recipes.rb
+++ Formula/esopipe-vcam-recipes.rb
@@ -10,6 +10,15 @@ class EsopipeVcamRecipes < Formula
     regex(/href=.*?vcam-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-vcam-recipes-2.3.17-4"
+    sha256 cellar: :any, arm64_tahoe:   "40ddfe504465c08c9b1e2a2e238dd7835cebd90347842d36f0cf8085f098189d"
+    sha256 cellar: :any, arm64_sequoia: "9a4dd74daead9d665df1fb34cf7dbefd3815e10d920d23a7b1b4ddd2a69168b2"
+    sha256 cellar: :any, arm64_sonoma:  "1e5c0555ba7d3a0bc79da250db69ae7dc707a1d263f51ef5533e340ca1c592ca"
+    sha256 cellar: :any, sonoma:        "7deeca0e87c0c7f025d7c25fc7a818ffed313581f788667db27b1096eb5327a6"
+    sha256 cellar: :any, x86_64_linux:  "bfcbf71113b31f5daac4ef89ca58f8b4fce70e605bf48d580ccfb2c7cd67e36d"
+  end
+
   def name_version
     "vcam-#{version.major_minor_patch}"
   end
