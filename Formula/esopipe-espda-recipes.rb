01f8991201a3b01c40cc5093afbdd01363cdfbb3
Stefano Zampieri <szampier@eso.org>
2026-06-26 12:22:25 +0200

    esopipe-espda-recipes 1.4.0-8

--- Formula/esopipe-espda-recipes.rb
+++ Formula/esopipe-espda-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeEspdaRecipes < Formula
   desc "ESO ESPRESSO-DAS instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso-das/espda-kit-1.4.0-7.tar.gz"
-  sha256 "0ebe856f384738491b63553166aed0a348d1b6abcc5276f2dcd70420197ef1ca"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso-das/espda-kit-1.4.0-8.tar.gz"
+  sha256 "7b887222570f4e86bdb8cb6d2f416dbb42209cf7fcd94885ce7c8fd983a6b021"
   license "GPL-2.0-or-later"
 
   livecheck do
