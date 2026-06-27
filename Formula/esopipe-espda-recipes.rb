95ac57011cededf97d71b5cb85c2b5433026610c
Stefano Zampieri <szampier@eso.org>
2026-06-27 19:19:59 +0200

    esopipe-espda-recipes 1.4.0-9
    Closes #536.
    
    Signed-off-by: Stefano Zampieri <56970291+szampier@users.noreply.github.com>

--- Formula/esopipe-espda-recipes.rb
+++ Formula/esopipe-espda-recipes.rb
@@ -1,8 +1,8 @@
 class EsopipeEspdaRecipes < Formula
   desc "ESO ESPRESSO-DAS instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso-das/espda-kit-1.4.0-8.tar.gz"
-  sha256 "7b887222570f4e86bdb8cb6d2f416dbb42209cf7fcd94885ce7c8fd983a6b021"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/espresso-das/espda-kit-1.4.0-9.tar.gz"
+  sha256 "4d1c273894c9f1212c06a8bb1d58166914ad09140ed8281439202a605c796999"
   license "GPL-2.0-or-later"
 
   livecheck do
