072a4925e4f08e8e17dbae1b11d79dc28b53f856
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2026-06-27 12:16:57 +0000

    esopipe-giraf-recipes: add 2.18.4-7 bottle.

--- Formula/esopipe-giraf-recipes.rb
+++ Formula/esopipe-giraf-recipes.rb
@@ -10,6 +10,15 @@ class EsopipeGirafRecipes < Formula
     regex(/href=.*?giraf-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-giraf-recipes-2.18.4-7"
+    sha256 cellar: :any, arm64_tahoe:   "38b278f89496ef8c17a1c7a3c954be8d79e5e94776df6486456df95e4b3a1b81"
+    sha256 cellar: :any, arm64_sequoia: "fd347c9959d3741b4f1f3c4f51a4527a63f4f193beef898f0696843e8008533a"
+    sha256 cellar: :any, arm64_sonoma:  "00bbf1f6319926d50dd30b6a4edfe1b3b4777b3c7db23e6b7cb900629dda0551"
+    sha256 cellar: :any, sonoma:        "4246c0e55adffa15931a51f585f615d6167b94e33c2f44ec3131dc41c64c6846"
+    sha256 cellar: :any, x86_64_linux:  "b67bb9a80e78cccc14e7c853d14639987b504af0f662584fdb06b95abf8b0e16"
+  end
+
   def name_version
     "giraf-#{version.major_minor_patch}"
   end
