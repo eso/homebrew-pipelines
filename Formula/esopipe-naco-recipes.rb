53aa5de6cedbf50f887c40f7d00a93ab120e54bc
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2026-06-27 13:16:55 +0000

    esopipe-naco-recipes: add 4.4.13-15 bottle.

--- Formula/esopipe-naco-recipes.rb
+++ Formula/esopipe-naco-recipes.rb
@@ -10,6 +10,15 @@ class EsopipeNacoRecipes < Formula
     regex(/href=.*?naco-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-naco-recipes-4.4.13-15"
+    sha256 cellar: :any, arm64_tahoe:   "946ce64b88465bd1c69b4bd296cef58697b988c3d4dfda6ebab84b1d5efcae10"
+    sha256 cellar: :any, arm64_sequoia: "92029883641f5b39cf62217c7a32e5ece05a7d55807584e7d84da84cff40f4a4"
+    sha256 cellar: :any, arm64_sonoma:  "19165190a6bf21ce9f070a7703c650dfd2f61742e1fbab8bef938c3daf9b2846"
+    sha256 cellar: :any, sonoma:        "19ddd831a6a9bcd60af14616998beb92b67a717f90b3cc845f90fca16a218107"
+    sha256 cellar: :any, x86_64_linux:  "0f7aae0b2acb79db137d3c6de177c50eec2f054790639d81f24d1dc69adc9b92"
+  end
+
   def name_version
     "naco-#{version.major_minor_patch}"
   end
