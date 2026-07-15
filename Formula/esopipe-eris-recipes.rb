738a15f7c5f4e3ccd0554aa34e55dcb4778f3039
Stefano Zampieri <56970291+szampier@users.noreply.github.com>
2026-07-15 16:11:37 +0000

    esopipe-eris-recipes: update 2.0.6-1 bottle.

--- Formula/esopipe-eris-recipes.rb
+++ Formula/esopipe-eris-recipes.rb
@@ -11,12 +11,12 @@ class EsopipeErisRecipes < Formula
   end
 
   bottle do
-    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-eris-recipes-2.0.0-2"
-    sha256 cellar: :any, arm64_tahoe:   "2afe3831ff005ae1c75cfb5d7f52da3d2af274115eae6950ef3d03fb52afdd61"
-    sha256 cellar: :any, arm64_sequoia: "0f77619c1c09edbc0b0b4513db0707e332d2b3b662cd2b6d4dff088e798e47e7"
-    sha256 cellar: :any, arm64_sonoma:  "8039cc20dec1e691eef3c36c675ea42bcccd23af761cb237223b5a3c9d551b69"
-    sha256 cellar: :any, sonoma:        "ee8804de668be654a2ea16fe953977a6ec1066c9a3acf090e289d4df6f9e03bc"
-    sha256 cellar: :any, x86_64_linux:  "63f98ba0677920172e1307d2effea644a4be984fb6df452933bd4f7535991a20"
+    root_url "https://github.com/eso/homebrew-pipelines/releases/download/esopipe-eris-recipes-2.0.6-1"
+    sha256 cellar: :any, arm64_tahoe:   "63937968c9fcecaa953e8e873aaf161d2860e0756fa1b9d9dc3e4d60845b513a"
+    sha256 cellar: :any, arm64_sequoia: "aea3f4d6e11a1c9434c27071eec2b4e45ba285f0e57d5fa4b094578f5d53ee92"
+    sha256 cellar: :any, arm64_sonoma:  "7e552279e9a47adb4f89c0973b320cce7a9c3fbf8fe2e0a1c6b4521fce843366"
+    sha256 cellar: :any, sonoma:        "4357a51cd2dfc9aa1928cc23929e8cbbbdf9250e6a05202f565c05cdaf81855d"
+    sha256 cellar: :any, x86_64_linux:  "a375f9b09ff92180ccb5d8214cd2ae8c2027ede408e6bd6fd80b03491d46d4a1"
   end
 
   def name_version
